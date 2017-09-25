aad8ac311a21f4f2bf61c7e291c6f54e406e1948
Eric <eric-k93@live.com>
2017-09-24 18:09:03 -0700

    Fixed some white space issues, still 2 lines in the ouput file need to be dealt with

--- client.py
+++ client.py
@@ -82,9 +82,9 @@ if READY_CHECK == 'READY':
             if 'ICS 200 HTML CONVERT COMPLETE' in STATE_3_DECODED:
                 STATE_3_DECODED = STATE_3_DECODED.split('ICS 200 HTML CONVERT COMPLETE')
                 STATE_3_DECODED = "".join(STATE_3_DECODED)
-                print(STATE_3_DECODED, end='\n')
+                print(STATE_3_DECODED, end='')
                 STATE = 4
             else:
-                print(STATE_3_DECODED, end="")
+                print(STATE_3_DECODED, end='')
 S1.close()
 S2.close()
